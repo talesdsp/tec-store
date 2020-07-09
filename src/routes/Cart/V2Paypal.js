cae835ca97cda98520e9f5bff4712ea8e80b81a1
tales <talesdsp@gmail.com>
2020-07-09 00:17:59 -0300

    change text

--- src/routes/Cart/V2Paypal.js
+++ src/routes/Cart/V2Paypal.js
@@ -1,10 +1,10 @@
-import React, {Component} from "react";
-import {PayPalButton} from "react-paypal-button-v2";
+import React, { Component } from "react"
+import { PayPalButton } from "react-paypal-button-v2"
 
 export default class V2Paypal extends Component {
   render() {
-    const checklist = this.props.checklist;
-    const total = this.props.total;
+    const checklist = this.props.checklist
+    const total = this.props.total
     // const history = this.props.history;
 
     return (
@@ -13,29 +13,28 @@ export default class V2Paypal extends Component {
           return actions.order.create({
             purchase_units: [
               {
-                description: `${[...checklist.items]}`,
+                description: `${checklist.items.map((v) => v.title + "\n")}`,
                 amount: {
                   currency: "BRL",
-                  value: total
-                }
-              }
-            ]
-          });
+                  value: total,
+                },
+              },
+            ],
+          })
         }}
         onApprove={(data, action) => {
           return action.order.capture().then((details) => {
-            alert("Transaction completed by " + details.payer.name.given_name);
-            alert("checklist: " + checklist.items);
-          });
+            alert("Transaction completed by " + details.payer.name.given_name)
+          })
         }}
         options={{
           clientId:
             "Ae7DfccDlJ5O9razBWUSukOJZ2sJjhihhDkG4lepNXT2cX08V32G5X6IqvrruAL8ycb-f1ZfzBDLuUk4",
-          currency: "BRL"
+          currency: "BRL",
         }}
-        onCancel={(data) => alert(data)}
-        onError={(err) => alert(err)}
+        onCancel={(data) => alert("You cancelled the payment")}
+        onError={(err) => alert("Ooops, something happened")}
       />
-    );
+    )
   }
 }
